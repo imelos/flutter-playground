833c50134d734f97f129c4519e5a2a9a7ca5ce34
pro <imelose@gmail.com>
2019-09-20 17:55:50 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -88,43 +88,47 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                   textDirection: TextDirection.ltr,
                   child: Text("hello world"),
                 ),
-                RotationTransition(
-                  turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
-                  child: Container(
-                    width: ScreenUtil.getInstance().setWidth(800),
-                    height: ScreenUtil.getInstance().setHeight(169),
-                    margin: EdgeInsets.all(30),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(66, 33, 33, 0.7),
-                          blurRadius: 10.0, // has the effect of softening the shadow
-                          spreadRadius: 3.0, // has the effect of extending the shadow
-                          offset: Offset(
-                            3.0, // horizontal, move right 10
-                            3.0, // vertical, move down 10
+                FadeTransition(
+                  opacity: Tween(begin: 0.0, end: 1.0).animate(_controller),
+                  child: RotationTransition(
+                      turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+                      child: Container(
+                        width: ScreenUtil.getInstance().setWidth(800),
+                        height: ScreenUtil.getInstance().setHeight(169),
+                        margin: EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(66, 33, 33, 0.7),
+                              blurRadius: 10.0, // has the effect of softening the shadow
+                              spreadRadius: 3.0, // has the effect of extending the shadow
+                              offset: Offset(
+                                3.0, // horizontal, move right 10
+                                3.0, // vertical, move down 10
+                              ),
+                            )
+                          ],
+                          image: DecorationImage(
+                            image: AssetImage("images/header.png"),
+                            fit: BoxFit.cover,
                           ),
-                        )
-                      ],
-                      image: DecorationImage(
-                        image: AssetImage("images/header.png"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        Directionality(
-                          textDirection: TextDirection.ltr,
-                          child: Text("hello world"),
                         ),
-                        Directionality(
-                          textDirection: TextDirection.ltr,
-                          child: Text("hello world2"),
+                        child: Row(
+                          children: <Widget>[
+                            Directionality(
+                              textDirection: TextDirection.ltr,
+                              child: Text("hello world"),
+                            ),
+                            Directionality(
+                              textDirection: TextDirection.ltr,
+                              child: Text("hello world2"),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  )
+                      )
+                  ),
                 )
+
               ],
             )
         )
