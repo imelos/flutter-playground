e8f75b09dfbd57578ef5e9689fc49d9a46e950c8
melos <melos@meloss-MacBook-Pro.local>
2019-09-21 18:03:57 +0300

    +

--- lib/src/auth/auth_model.dart
+++ lib/src/auth/auth_model.dart
@@ -3,7 +3,6 @@ class AuthModel {
   final String first;
   final String last;
   AuthModel(this.title, this.first, this.last);
-
   AuthModel.fromJson(Map<String, dynamic> json)
       : title = json["title"],
         first = json["first"],
--- lib/src/auth/auth_page.dart
+++ lib/src/auth/auth_page.dart
@@ -62,12 +62,29 @@ class _AuthPageState extends State<AuthPage> {
         ),
         child: Column(
           children: <Widget>[
+            GestureDetector(
+              // When the child is tapped, show a snackbar.
+              onTap: () {
+//                final snackBar = SnackBar(content: Text("Tap"));
+                  print('hello bi4');
+//                Scaffold.of(context).showSnackBar(snackBar);
+              },
+              // The custom button
+              child: Container(
+                padding: EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).buttonColor,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text('My Button'),
+              ),
+            ),
              StreamBuilder(
                  stream: bloc.subject.stream,
                  builder: (context, AsyncSnapshot<AuthModel> snapshot) {
                     return Text('test');
                  }
-        )
+             )
           ],
         ),
     );
