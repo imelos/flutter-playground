b54fd8958ec4a44e432ef0db0a85c7ca7a4bcb44
melos <melos@meloss-MacBook-Pro.local>
2019-09-21 11:53:23 +0300

    +

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flut_test/src/home/home_page.dart';
 import 'package:flut_test/src/auth/auth_page.dart';
 import 'package:fluro/fluro.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-//    ScreenUtil.instance = ScreenUtil(width: 2208, height: 1242)..init(context);
     Router router = new Router();
     router.define('auth', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
       return new AuthPage();
--- lib/src/auth/auth_page.dart
+++ lib/src/auth/auth_page.dart
@@ -30,11 +30,18 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 2208, height: 1242)..init(context);
     return Container(
         padding: EdgeInsets.all(25.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/asuka_love.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Directionality(
             textDirection: TextDirection.ltr,
-            child:Text("hello world",
+            child:Text("hello worlds",
               style: TextStyle(
                   color: Color.fromRGBO(255, 255, 255, 1)
               ),
