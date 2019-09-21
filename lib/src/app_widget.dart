65b4e7203ba0083c0bab0df6f10dfa46ca9e71a0
melos <melos@meloss-MacBook-Pro.local>
2019-09-22 01:54:16 +0300

    +

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flut_test/src/home/home_page.dart';
 import 'package:flut_test/src/auth/auth_page.dart';
 import 'package:fluro/fluro.dart';
-
+import 'package:flutter/services.dart';
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+
     Router router = new Router();
     router.define('auth', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
       return new AuthPage();
--- lib/src/shared/image_button/image_button_widget.dart
+++ lib/src/shared/image_button/image_button_widget.dart
@@ -1,30 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flut_test/src/shared/image_button/image_button_model.dart';
 
-class ImageButtonWidget extends StatelessWidget {
+class ImageButtonWidget extends StatefulWidget {
   final GestureTapCallback onPressed;
   final Data data;
-  ImageButtonWidget({this.data, @required this.onPressed});
+  final Function() notifyParent;
+  ImageButtonWidget({Key key, @required this.notifyParent, this.data, @required this.onPressed,}): super(key: key);
+
+  @override
+  _ImageButtonWidgetState createState() => _ImageButtonWidgetState();
+}
+
+class _ImageButtonWidgetState extends State<ImageButtonWidget> {
+//  String _currentImage = ImageButtonWidget.data.normal;
+  double _width = 30.00;
+
+  void _doSomething() {
+    // Using the callback State.setState() is the only way to get the build
+    // method to rerun with the updated state value.
+    setState(() {
+      _width = 20.00;
+    });
+  }
   @override
   Widget build(BuildContext context) {
+//    _currentMainImage = widget.data.normal;
+    AssetImage _image = new AssetImage(_currentImage);
+//     _width = widget.data.width;
 
+    return new Container(
+      child: GestureDetector(
+        onTapDown:(TapDownDetails details) {
+          _doSomething();
 
-    return GestureDetector(
-        onTap: () {
-          final snackBar = SnackBar(content: Text("Tap"));
-
-          Scaffold.of(context).showSnackBar(snackBar);
+//          setState(() {
+//            _image = new AssetImage(widget.data.pressed);
+//            _width = 33.00;
+//            print(_width);
+//          });
         },
-      child: Container(
-          width: ScreenUtil.getInstance().setWidth(data.width),
-          height: ScreenUtil.getInstance().setHeight(data.height),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(data.normal),
-              fit: BoxFit.contain,
+        child: Container(
+            width: ScreenUtil.getInstance().setWidth(_width),
+            height: ScreenUtil.getInstance().setHeight(ImageButtonWidget.data.height),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: _image,
+                fit: BoxFit.contain,
+              ),
             ),
-          )
+            child: Text('$_width'),
+        ),
       ),
     );
   }
