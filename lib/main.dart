1472f298baae5526f6bddc40a460987313b92bdc
melos <melos@meloss-MacBook-Pro.local>
2019-09-21 11:08:25 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flut_test/src/app_module.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flut_test/src/home/home_page.dart';
 
-void main() => runApp(AppModule());
+void main() {
+  Router router = new Router();
+
+  router.define('home', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return new HomePage();
+  }));
+
+  runApp(AppModule());
+}
