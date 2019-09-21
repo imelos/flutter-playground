fcfa6c7136265870236b643b455a06e3bba86d16
melos <melos@meloss-MacBook-Pro.local>
2019-09-21 11:39:24 +0300

    +

--- lib/src/auth/auth_page.dart
+++ lib/src/auth/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AuthPage extends StatefulWidget {
   @override
