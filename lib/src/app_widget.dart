0b750cf8f90191c0ef8977b74807a616b16bb57b
melos <melos@meloss-MacBook-Pro.local>
2019-09-21 11:15:03 +0300

    +

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flut_test/src/home/home_module.dart';
+import 'package:flut_test/src/home/home_page.dart';
+import 'package:flut_test/src/auth/auth_page.dart';
+import 'package:fluro/fluro.dart';
+
+
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Router router = new Router();
+
+    router.define('auth', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+      return new AuthPage();
+    }));
+
+    router.define('home', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+      return new HomePage();
+    }));
     return MaterialApp(
       title: 'Flutter Slidy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: HomeModule(),
+      onGenerateRoute: router.generator,
     );
   }
 }
--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -10,7 +10,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("shit"),
+        title: Text("home page"),
       ),
       body: Column(
         children: <Widget>[],
